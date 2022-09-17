74a2f6c265a2b68dc055c411ae460d381c591ea8
s-i27 <s_d27@icloud.com>
2022-09-18 00:37:17 +0900

    Update sqlgenarate.py

--- sqlgenarate.py
+++ sqlgenarate.py
@@ -1,35 +1,39 @@
 # code review
 import os
 
-' -- -----------------------------------------------------
-' -- Table `**`.`**`
-' -- -----------------------------------------------------
-' CREATE TABLE IF NOT EXISTS `**`.`**` (
-'   `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '**',
-'   `**` TINYINT(1) NOT NULL DEFAULT '0' COMMENT '**',
-'   `**` INT(11) NOT NULL DEFAULT '0' COMMENT '**',
-'   `**` int(11) NOT NULL DEFAULT '1' COMMENT '**',
-'   `**` varchar(255) NULL COMMENT '**',
-'   `**` text NOT NULL COMMENT '**',
-'   `**` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '**',
-'   `**` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '**',
-'   PRIMARY KEY (`id`))
-' ENGINE = InnoDB
-' DEFAULT CHARSET = utf8
-' COMMENT = '**';
-
 # DDL
 # path = os.getcwd()
 path = os.path.abspath('Sample.pu')
 print(path)
 
-DDL_template = 'CREATE TABLE IF NOT EXISTS #db_name#.#table_name# \n'\
-    '(col_name data_type,) \n'
+DDL_template = '''\
+' -- ----------------------------------------------------- \n'\
+' -- Table `#db_name#`.`#table_name#` \n'\
+' -- ----------------------------------------------------- \n'\
+' CREATE TABLE IF NOT EXISTS `#db_name#`.`#table_name#` (\n'\
+'   `#column#` INT(11) NOT NULL AUTO_INCREMENT COMMENT '**',\n'\
+'   `#column#` TINYINT(1) NOT NULL DEFAULT '0' COMMENT '**',\n'\
+'   `**` INT(11) NOT NULL DEFAULT '0' COMMENT '**',\n'\
+'   `**` int(11) NOT NULL DEFAULT '1' COMMENT '**',\n'\
+'   `**` varchar(255) NULL COMMENT '**',\n'\
+'   `**` text NOT NULL COMMENT '**',\n'\
+'   `**` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '**',\n'\
+'   `**` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '**',\n'\
+'   PRIMARY KEY (`id`))\n'\
+' ENGINE = InnoDB\n'\
+' DEFAULT CHARSET = utf8\n'\
+' COMMENT = '**';\n'\
+'''
+
+DDL_template_column_definitions = '''\
+'   `#column#` #type# #null# #default# COMMENT '#content#',\n'\
+'''
 
 f = open(path)
 DDL = [''] * 10
 for i, line in enumerate(f):
     print("/" + line.strip() + "/")
+    # DDL create
     if line.strip().find('entity'):
         replace_key = line[line.find('entity'):3]
         DDL[i] = DDL_template.replace('#table_name#', replace_key)
